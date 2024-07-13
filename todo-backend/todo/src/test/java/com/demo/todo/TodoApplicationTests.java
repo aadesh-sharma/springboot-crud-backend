7657c122dc6b61293779a5c7558254d8cb588168
Aadesh Sharma <76255777+aadesh-sharma@users.noreply.github.com>
2024-07-14 01:03:03 +0530

    Add files via upload

---
+++ todo-backend/todo/src/test/java/com/demo/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
